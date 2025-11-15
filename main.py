702250a807edc20934f1c74188d2f64cd0a2f117
alonso <malonso0109@gmail.com>
2025-11-14 20:54:01 -0500

    cambios agregados - nueva version

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def multiplicacion(a, b):
 
     return a*b
 
+def resta(a, b):
+
+    return a-b
+
 
 for l in lista_numeros:
     print( f'La multiplicacion de {l}x9 es igual a: {multiplicacion(l,9) }'     )
